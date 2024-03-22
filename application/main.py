eefdd35921f3fb6984297004a17ef13a1d45eced
gabodin <vgabriel2211@gmail.com>
2024-03-22 14:57:53 -0300

    Novas rotas

--- application/main.py
+++ application/main.py
@@ -7,7 +7,6 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-
 class Post(BaseModel):
     title: str
     content: str
@@ -23,6 +22,13 @@ def find_post(id):
     for post in my_posts:
         if post['id'] == id:
             return post
+        
+
+def find_index_post(id): 
+    for index, item in enumerate(my_posts):
+        if item["id"] == id:
+            return index
+
 
 @app.get('/')
 async def root():
@@ -61,6 +67,31 @@ def create_posts(post: Post):
     return {'data': post_dict}
 
 
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    post_index = find_index_post(id)
+
+    if not post_index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post #{id} was not found")
+    my_posts.pop(post_index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    post_index = find_index_post(id)
+
+    if post_index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post #{id} was not found")
+    
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[post_index] = post_dict
+
+    return {"data": post_dict}
 
 
 # title str, content str, category,
